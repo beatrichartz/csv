67ba71c7c433ae1aa47c3f7608400076958cc08d
Beat Richartz <beat.richartz@gmail.com>
2015-02-27 18:30:25 +0000

    initial commit

---
+++ mix.exs
@@ -0,0 +1,21 @@
+defmodule CSV.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+        app: :csv,
+        version: "0.1.0",
+        elixir: "~> 1.0",
+        package: package,
+        description: "CSV library"
+    ]
+  end
+
+  defp package do
+    [
+        contributors: ["Beat Richartz"],
+        licenses: ["MIT"],
+        links: [ { "GitHub", "https://github.com/beatrichartz/csv" } ]
+    ]
+  end
+end
