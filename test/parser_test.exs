1bd7f393c8b2e29f803dce00dd6c74e93ecaefa2
Beat Richartz <beat.richartz@gmail.com>
2016-02-21 13:49:06 +0000

    Cover stream halted in midst of escape sequence

--- test/parser_test.exs
+++ test/parser_test.exs
@@ -112,5 +112,32 @@ defmodule ParserTest do
     assert_receive {:syntax_error, {2, "Unterminated escape sequence."}}, 10
   end
 
+  test "raises a syntax error when halted in an escape sequence", context do
+    Enum.each [
+      {:start, 1},
+      {:content, "a"},
+      {:separator, ","},
+      {:double_quote, "\""},
+      {:content, "b"},
+      {:double_quote, "\""},
+      {:double_quote, "\""},
+      {:content, "c"},
+      {:separator, ","},
+      {:double_quote, "\""},
+      {:end, 1},
+      {:start, 2},
+      {:double_quote, "\""},
+      {:delimiter, "\r\n"},
+      {:content, "c"},
+      {:separator, ","},
+      {:content, "d"},
+      {:halt, 2}
+    ], fn(token) ->
+      send(context[:parser_pid], token)
+    end
+
+    assert_receive {:syntax_error, {2, "Stream halted with unterminated escape sequence."}}, 10
+  end
+
 
 end
