e246473b92913983055fae229dbb28d0c65abd5f
Beat Richartz <beat.richartz@gmail.com>
2016-08-06 17:42:55 +0100

    Add a maximum number of lines to be aggregated when escaping multilines. [fixes #45]

--- lib/csv/defaults.ex
+++ lib/csv/defaults.ex
@@ -5,11 +5,12 @@ defmodule CSV.Defaults do
 
   defmacro __using__(_) do
     quote do
-      @separator       ?,
-      @newline         ?\n
-      @carriage_return ?\r
-      @delimiter       << @carriage_return :: utf8 >> <> << @newline :: utf8 >>
-      @double_quote    ?"
+      @separator                  ?,
+      @newline                    ?\n
+      @carriage_return            ?\r
+      @delimiter                  << @carriage_return :: utf8 >> <> << @newline :: utf8 >>
+      @double_quote               ?"
+      @multiline_escape_max_lines 1000
     end
   end
 
--- test/line_aggregator_test.exs
+++ test/line_aggregator_test.exs
@@ -223,6 +223,13 @@ defmodule LineAggregatorTest do
     end
   end
 
+  test "fails if the multiline escape exceeds the maximum number of lines allowed to be aggregated" do
+    stream = Stream.map(["a,\"be\"\"", "c,d", "e,f", "g,h", "i,k", "\",b", "k,l,m"], &(&1))
+    assert_raise CorruptStreamError, fn ->
+      stream |> LineAggregator.aggregate(multiline_escape_max_lines: 2) |> Stream.run
+    end
+  end
+
   test "fails on open escape sequences with escaped quotes" do
     stream = Stream.map(["a,\"\"\"be\"\"", "\"\"c,d"], &(&1))
     assert_raise CorruptStreamError, fn ->
