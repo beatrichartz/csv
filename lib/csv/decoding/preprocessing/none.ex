aae85425cb7a4cf1077ab65c50993198b50bd14d
Beat Richartz <beat.richartz@gmail.com>
2017-01-14 11:56:43 +0000

    Add none preprocessor

---
+++ lib/csv/decoding/preprocessing/none.ex
@@ -0,0 +1,16 @@
+defmodule CSV.Decoding.Preprocessing.None do
+  use CSV.Defaults
+
+  @moduledoc ~S"""
+  The CSV none preprocessor module - input = output.
+  """
+
+  @doc """
+  Will pass on input unchanged
+  """
+
+  def process(stream, _ \\ []) do
+    stream
+  end
+
+end
