e32fc93da0136eb308c4759968d3018a389c2730
Malcolm Still <malcolm.still@gmail.com>
2017-11-02 15:11:54 -0500

    Fix indentation

--- lib/csv/defaults.ex
+++ lib/csv/defaults.ex
@@ -11,7 +11,7 @@ defmodule CSV.Defaults do
       @delimiter          << @carriage_return :: utf8 >> <> << @newline :: utf8 >>
       @double_quote       ?"
       @escape_max_lines   1000
-      @replacement           nil
+      @replacement        nil
     end
   end
 
