6cda08a317b13a07e5316f24806c7292541abfd5
Beat Richartz <beat.richartz@gmail.com>
2017-12-20 11:15:23 +0000

    Version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CSV.Mixfile do
   def project do
     [
         app: :csv,
-        version: "2.0.0",
+        version: "2.1.0",
         elixir: "~> 1.1",
         deps: deps(),
         package: package(),
