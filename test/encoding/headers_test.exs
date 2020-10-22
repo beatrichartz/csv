8712b92995684146a864fac6414645257ab10556
Michael Chu <michael.chu@validere.com>
2020-10-22 15:26:23 -0400

    Encode headers as Keyword list

--- test/encoding/headers_test.exs
+++ test/encoding/headers_test.exs
@@ -11,4 +11,19 @@ defmodule EncodingTests.HeadersTest do
     result = Encoder.encode([%{"c" => 1, "b" => 2}], headers: ["c", "b", "a"]) |> Enum.to_list()
     assert result == ["c,b,a\r\n", "1,2,\r\n"]
   end
+
+  test "inserts value of keyword list from header param" do
+    result = Encoder.encode([%{a: 1, b: 2, c: 3}], headers: [a: "c", b: "b", c: "a"]) |> Enum.to_list()
+    assert result == ["c,b,a\r\n", "1,2,3\r\n"]
+  end
+
+  test "inserts value of keyword list from header param with extra columns" do
+    result = Encoder.encode([%{a: 1, b: 2, c: 3}], headers: [a: "c", b: "b", c: "a", d: "x"]) |> Enum.to_list()
+    assert result == ["c,b,a,x\r\n", "1,2,3,\r\n"]
+  end
+
+  test "inserts value of keyword list from header param with reordered columns" do
+    result = Encoder.encode([%{c: 3, b: 2, a: 1}], headers: [a: "c", b: "b", c: "a", d: "x"]) |> Enum.to_list()
+    assert result == ["c,b,a,x\r\n", "1,2,3,\r\n"]
+  end
 end
