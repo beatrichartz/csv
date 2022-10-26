a39fd577ba4b49885baa37f4b9a6ac078174f20d
Beat <beatrichartz@users.noreply.github.com>
2022-10-27 05:20:29 +1100

    Remove leftover default for :`strip_fields`

--- lib/csv/defaults.ex
+++ lib/csv/defaults.ex
@@ -15,7 +15,6 @@ defmodule CSV.Defaults do
       @escape_max_lines 10
       @replacement nil
       @force_escaping false
-      @strip_fields false
       @escape_formulas false
       @unescape_formulas false
       @escape_formula_start ["=", "-", "+", "@"]
